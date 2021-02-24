3fa79804506306d04da82a2079a69bed4d6772b8
Apenasgabs <gabers357@gmail.com>
2021-02-23 22:58:31 -0300

    Fim da primeira aula :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ExperienceBar } from './components/ExperienseBar';
+import './styles/global.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jaja coloco alguma coisa aqui 
-        </p>
-        <p>Made with ❤️ and React by   
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/apenasgabs/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >  Apenasgabs
-        </a>
-        </p>
-        
-      </header>
+    <div className="container">
+        <h1>Site em construção</h1>
+        <ExperienceBar />
     </div>
   );
 }
