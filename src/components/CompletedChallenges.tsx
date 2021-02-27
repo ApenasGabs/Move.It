8684d1458cd0ad8904a92fff79997333e653ec77
Apenasgabs <gabers357@gmail.com>
2021-02-26 23:15:39 -0300

    Fix: module fixes

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,9 +1,9 @@
 
-import styles from '../styles/components/CompletedChallenges.module.css' 
+import styles from '../styles/components/CompletedChallenges.module.css' ;
 
-export function CompletedChallenges(){
+export function CompletedChallenges() {
     return (
-        <div className={styles.completedChallengesContainer} >
+        <div className={styles.completedContainer} >
             <span>Desafios completos</span>
             <span>5</span>
         </div>
